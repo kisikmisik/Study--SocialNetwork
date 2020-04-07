9ff3068872c620eb8332a2e13f3d8423cbb1532a
Kisik Misik <700mmr@gmail.com>
2020-04-07 23:28:47 +0300

    divided css into modules, added some styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Content from './components/Content';
 
-
 function App() {
   return (
     <div className="page-wrapper">
